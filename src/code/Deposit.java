f4c8c1d5ba1c3e3f991addb36851d00e183e2bf7
Dodomp <106491543+Dodomp@users.noreply.github.com>
2023-11-21 11:40:23 +0100

    double to Euro

--- src/code/Deposit.java
+++ src/code/Deposit.java
@@ -51,7 +51,7 @@ public class Deposit extends Transaction
                "enclosed cash and your checks clear." );
             
             // credit account to reflect the deposit
-            bankDatabase.credit( getAccountNumber(), amount ); 
+            bankDatabase.credit( getAccountNumber(), new Euro(amount) ); 
          } // end if
          else // deposit envelope not received
          {
